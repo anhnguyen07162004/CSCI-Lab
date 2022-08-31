e748c0d674ea905d0baa003a542726029801a7f6
anhnguyen07162004 <112536201+anhnguyen07162004@users.noreply.github.com>
2022-08-30 23:38:37 -0600

    Create Assessment 01B.py

---
+++ Assessment 01B.py
@@ -0,0 +1,15 @@
+# Anh Nguyen
+# CSCI 102 – Section B
+# Assessment 01B
+# No references
+# Time: 7 minutes
+
+
+print("Input the total length of fencing available (in yards).")
+
+length_yards = int(input("LENGTH> "))
+
+length_feet = length_yards * 3
+
+print("The maximum rectangular area that can be bound (in feet) is:")
+print(round(length_feet, 1))
