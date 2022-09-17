de28f99f0135834d00c27fb165cf01b51cd18789
anhnguyen07162004 <112536201+anhnguyen07162004@users.noreply.github.com>
2022-09-16 23:06:06 -0600

    Update Python Assessment 1.py

--- Python Assessment 1.py
+++ Python Assessment 1.py
@@ -35,6 +35,18 @@ if hours_input - hours <= 0:
         print("OUTPUT", "0" + str(hours2), mins)
       else:
         print("OUTPUT", hours2, mins)
+  elif early == mins_input:
+    mins = "00"
+    if hours2 < 10:
+      print("OUTPUT", "0" + str(hours2), mins)
+    else:
+      print("OUTPUT", hours2, mins)
+  else:
+    mins = mins_input - early
+    if hours2 < 10:
+      print("OUTPUT", "0" + str(hours2), "0" + str(mins))
+    else:
+      print("OUTPUT", hours2, "0" + str(mins))
 else:
   hours3 = hours_input
   if early > mins_input:
@@ -53,3 +65,15 @@ else:
         print("OUTPUT", "0" + str(hours3), mins)
       else:
         print("OUTPUT", hours3, mins)
+  elif early == mins_input:
+    mins = "00"
+    if hours3 < 10:
+      print("OUTPUT", "0" + str(hours3), mins)
+    else:
+      print("OUTPUT", hours3, mins)
+  else:
+    mins = mins_input - early
+    if hours3 < 10:
+      print("OUTPUT", "0" + str(hours3), "0" + str(mins))
+    else:
+      print("OUTPUT", hours3, "0" + str(mins))
