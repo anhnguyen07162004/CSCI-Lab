68ca60710a8dd4b6311f1d95a7f3405b4436fff5
anhnguyen07162004 <112536201+anhnguyen07162004@users.noreply.github.com>
2022-09-27 20:35:18 -0600

    Update Assessment 05B.py

--- Assessment 05B.py
+++ Assessment 05B.py
@@ -2,11 +2,10 @@
 # CSCI 102 - Section B
 # Assessment 05A
 # References: None
-# Time: 25 minutes
+# Time: 40 minutes
 
-# Initial variable to track total amount drank
-total_drank = 0
-total_full = 0
+total_drank = 0 # Initial variable to track total amount drank
+extra = 0 # Will be used to track remainder
 result = 0 # Will be used later to help with calculations
 
 # Getting inputs
@@ -14,14 +13,14 @@ empty = int(input("EMPTIES> "))
 found_empty = int(input("FOUND> "))
 cost = int(input("COST> "))
 total_empty = empty + found_empty # Combining the amount of empty cans 
+index = 0 # Prevents and infinite loop
 
 # Processing the amount Blaster can drink
-while total_empty != 0:
-  if total_empty < cost:
-    break
-  else:
-    result = total_empty // cost
-    total_empty = total_empty - result
-    total_drank += result
+while total_empty != 0 and index < 999:
+  extra = total_empty % cost
+  result = total_empty // cost
+  total_empty = result + extra
+  total_drank += result
+  index += 1
 
 print("OUTPUT ", total_drank)
